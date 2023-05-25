85e592cdf80ad284c16c52017677135e883018ea
AndiMSF <andimuhammadsatriafadhil77@gmail.com>
2023-05-25 18:20:57 +0700

    Verify Token and Revising some endpoint

--- README.md
+++ README.md
@@ -16,14 +16,14 @@
 #   "email": "",
 #   "password": "harus jumlah 6 atau lebih",
 # }
-# https://beginners-project-69.et.r.appspot.com/api/signup
+# https://beginners-project-69.et.r.appspot.com/api/auth/signup
 
 # ini *POST* untuk signin
 # {
 #  "email": "", 
 #   "password": "",
 # }
-# https://beginners-project-69.et.r.appspot.com/api/signin
+# https://beginners-project-69.et.r.appspot.com/api/auth/signin
 
 # ini *POST* untuk update profile
 # {
@@ -34,3 +34,95 @@
 #  "alamat": ""
 # }
 # https://beginners-project-69.et.r.appspot.com/api/profile/:id ( *:id di replace sama id nya user* )
+
+
+
+
+
+Standard API
+{root.api}/v1/{grouping}/{endpoint}
+
+SAMPLE:
+https://beginners-project-69.et.r.appspot.com/v1/auth/login
+
+Group : Authentication
+[1] - Signup
+*POST*
+https://beginners-project-69.et.r.appspot.com/v1/auth/signup
+
+req : 
+{
+    "name": "",
+    "email": "",
+    "password": "",
+    "phoneNumber": ""
+}
+
+res :
+{
+
+}
+
+Group: Homepage
+[1] Homepage
+*GET*
+https://beginners-project-69.et.r.appspot.com/v1/:id/homepage
+
+res :
+{
+    "name": "",
+    "workplace": "",
+    "job_title": ""
+}
+
+[2] User detail
+*GET*
+https://beginners-project-69.et.r.appspot.com/v1/:id/homepage/:id
+
+res :
+{
+    "name": "",
+    "workplace": "",
+    "job_title": "",
+}
+
+[3] Profile
+*GET*
+https://beginners-project-69.et.r.appspot.com/v1/profile
+
+res : 
+{
+
+}
+
+*POST*
+https://beginners-project-69.et.r.appspot.com/v1/profile
+
+req : {
+
+}
+
+[4] Card Storage
+*GET*
+https://beginners-project-69.et.r.appspot.com/v1/card-storage
+
+req :
+{
+
+}
+
+res :
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser')
 // Setup for service account initializing node js to connect to firebase API
 const admin = require('firebase-admin')
 const credentials = require('./key.json')
+const response = require('./response')
+
 
 const app = express()
 admin.initializeApp({
     credential: admin.credential.cert(credentials),
-    // Realtime DB URL
-    databaseURL: 'https://beginners-project-69-default-rtdb.asia-southeast1.firebasedatabase.app/',
 })
 // Initialize database
 // firestore
@@ -23,8 +23,15 @@ app.use(bodyParser.json());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+// Verify Id Token
+function verifyIdToken(idToken) {
+    return admin.auth().verifyIdToken(idToken)
+}
+
+
 // SignUp
-app.post('/api/signup', async (req,res) =>{
+app.post('/v1/auth/signup', async (req,res) =>{
+    try {
     // Authentication
     const userProperty = {
         name: req.body.name,
@@ -32,16 +39,17 @@ app.post('/api/signup', async (req,res) =>{
         email: req.body.email,
         password: req.body.password,
     }
-
     const additionalInfo = {
-          // additional info
-          phoneCompany : "",
-          phoneFax : "",
-          workplace_uri : "",
+          // additional info Company
+          job_title: "",
           workplace : "",
-          alamat : "",
+          addressCompany : "",
+          emailCompany: "",
+          phoneTelpCompany:"",
+          phoneFaxCompany : "",
+          phoneMobileCompany : "",
+          workplace_uri : "",  
     }
-
     const userCreate = await admin.auth().createUser({
         name: userProperty.name,
         phoneNumber: userProperty.phoneNumber,
@@ -51,118 +59,142 @@ app.post('/api/signup', async (req,res) =>{
         emailVerified: false,
         disabled: false,
     })
-
-    console.log(userProperty.password);
     const salt = await bcrypt.genSalt(10)
     const hashedPassword =  await bcrypt.hash(userProperty.password, salt)
     userProperty.password = hashedPassword
 
     // Storing user data to realtime database
-    const usersRef = admin.database().ref('users/').child(userCreate.uid).set({
+    const usersRef = admin.firestore().collection('users/').doc(userCreate.uid).set({
         name: userProperty.name,
         phoneNumber: userProperty.phoneNumber,
         email: userProperty.email,
         password: hashedPassword,
          // additional info
-         phoneCompany : additionalInfo.phoneCompany,
-         phoneFax : additionalInfo.phoneFax,
-         workplace_uri : additionalInfo.workplace_uri,
-         workplace : additionalInfo.workplace,
-         alamat : additionalInfo.alamat,
+        job_title:additionalInfo.job_title,
+        workplace : additionalInfo.workplace,
+        addressCompany : additionalInfo.addressCompany,
+        emailCompany: additionalInfo.emailCompany,
+        phoneTelpCompany: additionalInfo.phoneTelpCompany,
+        phoneFaxCompany : additionalInfo.phoneFaxCompany,
+        phoneMobileCompany : additionalInfo.phoneMobileCompany,
+        workplace_uri : additionalInfo.workplace_uri,
     })
         .then(() => {
-            console.log(userProperty.password)
-            console.log(userCreate.password);
-            console.log('User data stored successfully');
-            res.status(200).json({ message: 'User created successfully!', user: usersRef});
+            response(201,usersRef,"User Created Successfully",res)
         })
-        .catch((error) => {
-            console.log('Error storing user data:',error);
-            res.status(500).json({ error: 'Failed to create user.' });
-        })
+    }catch(error) {
+            response(400,error,"Failed To Create User",res)
+        }
    
-    })    
-
-
+})  
 
 // Homepage
-app.get('/api/homepage', async (req,res) => { 
+app.get('/v1/homepage', async (req,res) => { 
     try {
         // Ambil collection / table namenya
         const usersCollection = db.collection('usersContact')
         // Method get() untuk mengambil isi dari collection / table
         const getCollection = await usersCollection.get();
-
         // array kosong untuk menyimpan data dari getCollection, karena getCollection itu isinya banyak,
         // bukan hanya data saja, jadi disini kita mengambil data nya aja untuk di simpan di array kosong dibawah.
         let getCollectionData = []  
-
         getCollection.forEach(allFiles => {
             getCollectionData.push(allFiles.data())
+            
         });
-        res.send(getCollectionData);
         console.log(getCollectionData)
-    } catch (error) {
-        console.log(error);
+        response(200,getCollectionData,"Saved User",res)
+
+    } catch (error) {   
+        console.log(error)
+        response(500,error,"No Saved User",res)
+
     }
 })
 
-// Profile berdasarkan Specific ID
-app.get('/api/:id/homepage', async (req,res) => {
+// user detail
+app.get('/v1/user-detail/:id', async (req,res) => {
         try {
-            // Ambil collection / table namenya
-            const usersCollection = db.collection('usersContact').doc(req.params.id)
-            // Method get() untuk mengambil isi dari collection / table
-            const getCollection = await usersCollection.get();
+            const authHeader = req.headers.authorization
     
+            if(!authHeader || !authHeader.startsWith('Bearer ')) {
+                return res.status(403).send('Unauthorized')
+            }
+            const idToken = authHeader.split('Bearer ') [1]
+            verifyIdToken(idToken)
+            .then((decodedToken) => {
+                const userId = decodedToken.uid
+                
+            // Ambil collection / table namenya
+            const usersCollection = db.collection('usersContact').doc(userId)
+            // Method get() untuk mengambil isi dari collection / table
+            const getCollection = usersCollection.get(); 
             // karena cuma 1 namecard jadi kita tidak perlu lakukan for loop untuk mengambil data
             // cukup langsung tambahkan .data() di getCollection.
-            res.send(getCollection.data());
+            response(200,getCollection.data(),"User Created Successfully",res)
+            })
         } catch (error) {
-            console.log(error);
+            response(500,error,"No user to see",res)
         }
 })
 
 // Profile kita
-app.get('/api/:id/profile', async (req,res) => {
+app.get('/v1/profile', async (req,res) => {
+ 
+    const authHeader = req.headers.authorization
+    
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).send('Unauthorized')
+    }
 
+    const idToken = authHeader.split('Bearer ') [1]
 
-          const realtimeDb =  admin.database().ref('users/').child(req.params.id).once('value') 
-            .then((snapshot) => {
-            // Access the data snapshot
-            const data = snapshot.val();
-            res.send(data)
-            console.log(data);
-          })
-          .catch((error) => {
-            console.error(error);
-          })
-
-})
-// Update Profile kita
-app.post('/api/:id/profile', async (req,res) => {
-    try {
-        const realtimeDB = admin.database().ref('users/').child(req.params.id).update({
-            // emailCompany: req.body.emailCompany,
-            // phoneCompanyMobile : req.body.phoneCompanyMobile,
-            phoneCompany: req.body.phoneCompany,
-            phoneFax: req.body.phoneFax,
-            workplace_uri: req.body.workplace_uri,
-            workplace: req.body.workplace,
-            alamat: req.body.alamat
+    verifyIdToken(idToken)
+    .then((decodedToken) => {
+        const userId = decodedToken.uid
+        const usersRef =  db.collection('users/').doc(userId).get() 
+        .then((doc) => {
+        if (doc.exists) {
+            usersData = doc.data()
+            response(200,usersData,"Berikut profile user",res)
+            }
         })
 
-        res.status(200).json({ message: 'User updated successfully!'});
-        console.log(realtimeDB);
+    })
+});
        
-      
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Failed to update User!'});
 
+
+// Update Profile kita
+app.post('/v1/profile/', async (req,res) => {
+    
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).send('Unauthorized')
     }
-})
+    const idToken = authHeader.split('Bearer ') [1]
+    verifyIdToken(idToken)
+    .then((decodedToken) => {
+        const userId = decodedToken.uid
+          const realtimeDB = admin.firestore().collection('users/').doc(userId).update({
+            job_title: req.body.job_title,
+            workplace: req.body.workplace,
+            addressCompany: req.body.addressCompany,
+            emailCompany: req.body.emailCompany,
+            phoneTelpCompany : req.body.phoneTelpCompany,
+            phoneFaxCompany: req.body.phoneFaxCompany,
+            phoneMobileCompany: req.body.phoneMobileCompany,
+            workplace_uri: req.body.workplace_uri,
+        })
+        response(200,realtimeDB,"User updated",res)
+    }) .catch((error) => {
+        response(400,error,"Failed to update user",res)
+    })
 
+
+
+   
+})
 
 // Server running on port ....
 const PORT = process.env.PORT || 8080
